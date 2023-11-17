eb069e89e271a6afe2af90123cc461c0ab36d7c7
K0rosu <craft.seto@gmail.com>
2023-11-17 15:39:40 -0300

    atividade do felipao

--- index.js
+++ index.js
@@ -1,30 +1,50 @@
-// // Nome e a quantidade de experiência (XP) do herói
-// var nome = "Marcelo";
-// var xp = 100000;
+const readline = require('readline');
 
-// // Estrutura de decisão para determinar o nível do herói
-// var nivel;
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
 
-// if (xp < 1000) {
-//     nivel = "Ferro";
-// } else if (xp >= 1001 && xp <= 2000) {
-//     nivel = "Bronze";
-// } else if (xp >= 2001 && xp <= 5000) {
-//     nivel = "Prata";
-// } else if (xp >= 6001 && xp <= 7000) {
-//     nivel = "Ouro";
-// } else if (xp >= 7001 && xp <= 8000) {
-//     nivel = "Platina";
-// } else if (xp >= 8001 && xp <= 9000) {
-//     nivel = "Ascendente";
-// } else if (xp >= 9001 && xp <= 10000) {
-//     nivel = "Imortal";
-// } else {
-//     nivel = "Radiante";
-// }
+// Nome e a quantidade de experiência (XP) do herói
+let nome, xp;
 
-// // Mensagem com o nome e o nível do herói
-// console.log("O Herói de nome " + nome + " está no nível de " + nivel);
+rl.question('Digite o nome do seu herói: ', (answerNome) => {
+  nome = answerNome;
+
+  rl.question('Digite a quantidade de xp: ', (answerXP) => {
+    xp = parseInt(answerXP);
+
+    // Estrutura de decisão para determinar o nível do herói
+    let nivel;
+
+    if (xp < 1000) {
+      nivel = "Ferro";
+    } else if (xp >= 1001 && xp <= 2000) {
+      nivel = "Bronze";
+    } else if (xp >= 2001 && xp <= 5000) {
+      nivel = "Prata";
+    } else if (xp >= 6001 && xp <= 7000) {
+      nivel = "Ouro";
+    } else if (xp >= 7001 && xp <= 8000) {
+      nivel = "Platina";
+    } else if (xp >= 8001 && xp <= 9000) {
+      nivel = "Ascendente";
+    } else if (xp >= 9001 && xp <= 10000) {
+      nivel = "Imortal";
+    } else {
+      nivel = "Radiante";
+    }
+
+    // Mensagem com o nome e o nível do herói
+    console.log("O Herói de nome " + nome + " está no nível de " + nivel);
+
+    // Fecha a interface de leitura
+    rl.close();
+  });
+});
+
+
+
 
 
 // let resultado = calcularNivel(75, 25);
@@ -55,39 +75,39 @@
 
 
 
-class Hero {
-    constructor(nome, idade, tipo) {
-      this.nome = nome;
-      this.idade = idade;
-      this.tipo = tipo;
-    }
+// class Hero {
+//     constructor(nome, idade, tipo) {
+//       this.nome = nome;
+//       this.idade = idade;
+//       this.tipo = tipo;
+//     }
   
-    atacar() {
-      let ataque;
-      switch (this.tipo) {
-        case 'mago':
-          ataque = 'usou magia';
-          break;
-        case 'guerreiro':
-          ataque = 'usou espada';
-          break;
-        case 'monge':
-          ataque = 'usou artes marciais';
-          break;
-        case 'ninja':
-          ataque = 'usou shuriken';
-          break;
-        default:
-          ataque = 'usou um ataque desconhecido';
-      }
+//     atacar() {
+//       let ataque;
+//       switch (this.tipo) {
+//         case 'mago':
+//           ataque = 'usou magia';
+//           break;
+//         case 'guerreiro':
+//           ataque = 'usou espada';
+//           break;
+//         case 'monge':
+//           ataque = 'usou artes marciais';
+//           break;
+//         case 'ninja':
+//           ataque = 'usou shuriken';
+//           break;
+//         default:
+//           ataque = 'usou um ataque desconhecido';
+//       }
   
-      console.log(`o ${this.tipo} atacou usando ${ataque}`);
-    }
-  }
+//       console.log(`o ${this.tipo} atacou usando ${ataque}`);
+//     }
+//   }
   
-  let heroi = new Hero('Gandalf', 100, 'mago');
-  heroi.atacar();
+//   let heroi = new Hero('Gandalf', 100, 'mago');
+//   heroi.atacar();
   
-  let heroi2 = new Hero('Aragorn', 35, 'guerreiro');
-  heroi2.atacar();
+//   let heroi2 = new Hero('Aragorn', 35, 'guerreiro');
+//   heroi2.atacar();
   
